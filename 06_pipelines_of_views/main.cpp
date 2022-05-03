dc7d5af918587d5034daf864b31a1a4fac4e0069
Vladislav Antonov <vladislav.antonov@gmail.com>
2022-05-03 09:10:15 +0300

    Add Pipelines of Views examples

---
+++ 06_pipelines_of_views/main.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <ranges>
+
+void print(const std::ranges::input_range auto& rg) {
+  for (const auto& value : rg) {
+    std::cout << value << '\n';
+  }
+}
+
+struct Coord {
+  double x, y, z;
+
+  auto operator<=>(const Coord&) const = default;
+};
+
+std::ostream& operator<<(std::ostream& out, const Coord& data) {
+  out << "{" << data.x << "," << data.y << "," << data.z << "}";
+  return out;
+}
+
+int main() {
+  std::array points{Coord{1, 2, 3}, Coord{4, 5, 6}, Coord{0, 2, 0},
+                    Coord{0, 0, 2}};
+  std::ranges::sort(points);
+  print(points | std::views::drop(1));
+  return 0;
+}
