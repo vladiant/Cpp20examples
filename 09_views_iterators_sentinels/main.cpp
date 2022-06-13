40ffdf6159b1d71dcf67ae29ebb855b6293bba5e
Vladislav Antonov <vladislav.antonov@gmail.com>
2022-06-13 08:01:58 +0300

    Add views iterators sentinels example

---
+++ 09_views_iterators_sentinels/main.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <ranges>
+
+void print(std::ranges::input_range auto&& rg) {
+  for (const auto& value : rg) {
+    std::cout << value << '\n';
+  }
+}
+
+template <auto Val>
+struct EndValue {
+  bool operator==(auto pos) const {
+    return *pos == Val;  // Val is the end value
+  }
+};
+
+struct Coord {
+  double x, y, z;
+
+  auto operator<=>(const Coord&) const = default;
+};
+
+std::ostream& operator<<(std::ostream& out, const Coord& data) {
+  out << "{" << data.x << "," << data.y << "," << data.z << "}";
+  return out;
+}
+
+int main() {
+  std::array points{Coord{1, 2, 3}, Coord{4, 5, 6}, Coord{0, 2, 0},
+                    Coord{0, 0, 2}};
+  std::ranges::sort(points);
+  print(std::ranges::subrange{points.begin(), EndValue<Coord{4, 5, 6}>{}});
+  return 0;
+}
