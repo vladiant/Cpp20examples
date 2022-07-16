bfa47e63d096a6ca9da0dd855954325d4c85918b
Vladislav Antonov <vladislav.antonov@gmail.com>
2022-07-16 10:16:31 +0300

    Add skip typename example

---
+++ 12_skip_typename/main.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <vector>
+
+template <typename T, typename... Args>
+auto mergeValue(const T& rg, Args&&... vals) {
+  // Create compile time vector
+  std::vector<T::value_type> v{rg.begin(), rg.end()};
+
+  (..., v.push_back(std::forward<Args>(vals)));  // merge passed values
+
+  std::sort(v.begin(), v.end());  // sort all elements
+
+  // return extended collection
+  return v;
+}
+
+int main() {
+  // Compile time initialization of array
+  constexpr std::array orig{0, 8, 15, 132, 4, 77, 3};
+
+  // Compile time initialization of sorted extended array
+  auto merged = mergeValue(orig, 42, 4);
+  for (const auto& i : merged) {
+    std::cout << i << ' ';
+  }
+
+  return 0;
+}
