c467841b95eddb44ae78c6f2a200a810090b3ee1
Vladislav Antonov <vladislav.antonov@gmail.com>
2022-12-07 08:43:32 +0200

    Add auto function arguments sample

---
+++ 21_auto_function_parameters/main.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+
+void add(auto& col, const auto& val)
+{
+  col.push_back(val);
+}
+
+int main() {
+  std::vector<int> col;
+  
+  add(col, 42); 
+
+  for (const auto& i : col) {
+    std::cout << i << ' ';
+  }
+
+  return 0;
+}
