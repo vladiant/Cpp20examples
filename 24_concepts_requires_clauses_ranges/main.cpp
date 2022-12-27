186f67152b2f1f437e8b305479bca922de1a5d5f
Vladislav Antonov <vladislav.antonov@gmail.com>
2022-12-27 09:55:46 +0200

    Add Concepts with Ranges sample

---
+++ 24_concepts_requires_clauses_ranges/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <ranges>
+#include <set>
+#include <vector>
+
+template <typename Coll>
+concept HasPushBack = requires(Coll c, std::ranges::range_value_t<Coll> v) {
+  c.push_back(v);
+};
+
+void add(
+    auto& col,
+    const auto& val) requires HasPushBack<std::remove_cvref_t<decltype(col)>> {
+  col.push_back(val);
+}
+
+void add(auto& col, const auto& val) { col.insert(val); }
+
+int main() {
+  std::vector<int> col1;
+  std::set<int> col2;
+
+  add(col1, 42);
+  add(col2, 42);
+
+  for (const auto& i : col1) {
+    std::cout << i << ' ';
+  }
+  for (const auto& i : col2) {
+    std::cout << i << ' ';
+  }
+
+  return 0;
+}
