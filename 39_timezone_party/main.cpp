a0135dafc1ae495988b0002b42baa659ee76e560
Vladislav Antonov <vladislav.antonov@gmail.com>
2023-04-15 11:32:02 +0200

    Add Time Zone sample

---
+++ 39_timezone_party/main.cpp
@@ -0,0 +1,17 @@
+#include <chrono>
+#include <iostream>
+
+using namespace std::chrono;
+
+int main() {
+  //auto firstDay = 2021y / 1 / 31;
+  auto firstDay = 2021y/1/std::chrono::last;
+
+  auto lastDay = 12 / 31d / 2021;
+
+  for (auto d = firstDay; d <= lastDay; d += std::chrono::months{1}) {
+    std::cout << d.ok() << ": Party\n";
+  }
+
+  return 0;
+}
