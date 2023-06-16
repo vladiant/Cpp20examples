9243af108edfe1512cd5a6a36823e62b38ee6f7f
Vladislav Antonov <vladislav.antonov@gmail.com>
2023-06-16 21:21:02 +0300

    Add concepts type constaints sample

--- 22_concepts_type_constraints/main.cpp
+++ 22_concepts_type_constraints/main.cpp
@@ -3,25 +3,27 @@
 #include <vector>
 
 template <typename Coll>
-concept HasPushBack = requires(Coll c, Coll::value_type v) { c.push_back(v); };
+concept HasPushBack = requires(Coll c, typename Coll::value_type v) {
+  c.push_back(v);
+};
 
 void add(HasPushBack auto& col, const auto& val) { col.push_back(val); }
 
 void add(auto& col, const auto& val) { col.insert(val); }
 
 int main() {
-    std::vector<int> col1;
-    std::set<int> col2;
+  std::vector<int> col1;
+  std::set<int> col2;
 
-    add(col1, 42);
-    add(col2, 42);
+  add(col1, 42);
+  add(col2, 42);
 
-    for (const auto& i : col1) {
-        std::cout << i << ' ';
-    }
-    for (const auto& i : col2) {
-        std::cout << i << ' ';
-    }
+  for (const auto& i : col1) {
+    std::cout << i << ' ';
+  }
+  for (const auto& i : col2) {
+    std::cout << i << ' ';
+  }
 
-    return 0;
+  return 0;
 }
