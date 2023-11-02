517997181a4538a07fe9fc4361b92a920058c019
Vladislav Antonov <vladislav.antonov@gmail.com>
2023-11-02 08:44:43 +0200

    Add interrupt_jthread sample

---
+++ 2023/interrupt_jthread/interrupt_jthread.cpp
@@ -0,0 +1,38 @@
+#include <chrono>
+#include <iostream>
+#include <stop_token>
+#include <thread>
+
+using namespace ::std::literals;
+
+int main() {
+  std::cout << '\n';
+
+  std::jthread nonInterruptable([] {
+    int counter{0};
+    while (counter < 10) {
+      std::this_thread::sleep_for(0.2s);
+      std::cerr << "nonInterruptable: " << counter << '\n';
+      ++counter;
+    }
+  });
+
+  std::jthread interruptable([](std::stop_token stoken) {
+    int counter{0};
+    while (counter < 10) {
+      std::this_thread::sleep_for(0.2s);
+      if (stoken.stop_requested()) return;
+      std::cerr << "interruptable: " << counter << '\n';
+      ++counter;
+    }
+  });
+
+  std::this_thread::sleep_for(1s);
+
+  std::cerr << '\n';
+  std::cerr << "Main thread interrupts both jthreads" << std::endl;
+  nonInterruptable.request_stop();
+  interruptable.request_stop();
+
+  std::cout << '\n';
+}
