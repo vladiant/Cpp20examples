581970a4f7c0903326657613848df826bf4e5d05
Vladislav Antonov <vladislav.antonov@gmail.com>
2024-04-24 20:21:24 +0300

    Separate sequencced output

---
+++ concurrency/sequenced_output_streams/sequenceOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <syncstream>
+int main() {
+  std::osyncstream bout1(std::cout);
+  bout1 << "Hello, ";
+  {
+    std::osyncstream(bout1.get_wrapped()) << "Goodbye, "
+                                          << "Planet!" << '\n';
+  }  // emits the contents of the temporary buffer
+
+  bout1 << "World!" << '\n';
+
+}  // emits the contents of bout1
