99bf5048bb5baf590b7fb4adadf9e8c58eabd4c5
Vladislav Antonov <vladislav.antonov@gmail.com>
2024-04-27 19:30:18 +0300

    Add erase upper example

---
+++ 2023/erase_upper/erase_upper.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string str{"Only For TesTing PurPose."};
+  std::cout << "str: " << str << '\n';
+
+  std::erase_if(str, [](char c) { return std::isupper(c); });
+  std::cout << "str: " << str << '\n';
+}
