0cc539a3b427942f1dcf861a21e15a3778a87903
Vladislav Antonov <vladislav.antonov@gmail.com>
2025-09-16 21:07:43 +0300

    Add basic co_return example

---
+++ coroutines/basic_examples/void_co_return/main.cpp
@@ -0,0 +1,23 @@
+// https://www.youtube.com/watch?v=b6pYieNd_OY
+// C++ Coroutines - Gods from the Machine - Phil Nash - ACCU 2025
+
+#include <coroutine>
+#include <iostream>
+
+struct Task {
+  struct promise_type {
+    Task get_return_object() { return {}; }
+    auto initial_suspend() { return std::suspend_never{}; }
+    auto final_suspend() noexcept { return std::suspend_always{}; }
+    void unhandled_exception() { std::abort(); }
+    void return_void() {}
+  };
+};
+
+Task f() { co_return; }
+
+int main() {
+  f();
+
+  return 0;
+}
