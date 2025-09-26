a133e8bd2f7332be6a0bf34b093eeedd1c41012e
Vladislav Antonov <vladislav.antonov@gmail.com>
2025-09-26 08:13:28 +0300

    Add co_yield suspended loop

---
+++ coroutines/basic_examples/co_yield_suspended_loop/main.cpp
@@ -0,0 +1,119 @@
+#include <coroutine>
+#include <iostream>
+#include <source_location>  // ScopedLogger
+
+// https://github.com/philsquared/cpp_coroutines_helpers/
+// https://www.youtube.com/watch?v=b6pYieNd_OY
+// C++ Coroutines - Gods from the Machine - Phil Nash - ACCU 2025
+
+// Logs to cout, with current indent
+template <typename... Args>
+void log(Args&&... args);
+
+struct ScopedLogger {
+  inline static int indent;
+
+  std::string m_name;
+
+  ScopedLogger(
+      std::string name = std::source_location::current().function_name())
+      : m_name(std::move(name)) {
+    log("\\ ", m_name, '\n');
+    indent++;
+  }
+  ~ScopedLogger() {
+    indent--;
+    log("/ ", m_name, '\n');
+  }
+};
+
+template <typename... Args>
+void log(Args&&... args) {
+  std::cout << std::string(ScopedLogger::indent, ' ');
+  (std::cout << ... << std::forward<Args>(args));
+}
+
+// Creates a ScopedLogger for the current function
+#define LOGF() ScopedLogger logger##__LINE__
+
+struct Task {
+  struct promise_type {
+    Task get_return_object() {
+      LOGF();
+      return Task{std::coroutine_handle<promise_type>::from_promise(*this)};
+    }
+
+    auto initial_suspend() {
+      LOGF();
+      return std::suspend_never{};
+    }
+
+    auto final_suspend() noexcept {
+      LOGF();
+      return std::suspend_always{};
+    }
+
+    void unhandled_exception() {
+      LOGF();
+      std::abort();
+    }
+
+    void return_value(int aValue) {
+      LOGF();
+      mReturnedValue = aValue;
+    }
+
+    auto yield_value(int aValue) {
+      LOGF();
+      mYieldedValue = aValue;
+      // should be suspend_always to wait for the next co_yield
+      return std::suspend_always{};
+    }
+
+    int mReturnedValue{};
+    int mYieldedValue{};
+  };
+
+  // Cotrols and destroys coroutines
+  // Obtained by a promise
+  std::coroutine_handle<promise_type> handle;
+
+  ~Task() {
+    if (handle) {
+      handle.destroy();
+    }
+  }
+
+  auto operator=(Task&&) = delete;
+  auto operator=(const Task&) = delete;
+
+  int get_returned_value() const { return handle.promise().mReturnedValue; }
+
+  int get_yielded_value() const { return handle.promise().mYieldedValue; }
+
+  // Updated to verify there are values to yield
+  bool resume() {
+    handle.resume();
+    return handle.done();
+  }
+};
+
+Task f() {
+  LOGF();
+  co_yield 1;
+  co_yield 2;
+  co_yield 3;
+  co_return 42;
+}
+
+int main() {
+  LOGF();
+  auto task = f();
+
+  do {
+    log("YieldedValue ", task.get_yielded_value(), '\n');
+  } while (!task.resume());
+
+  log("ReturnedValue ", task.get_returned_value(), '\n');
+  return 0;
+}
