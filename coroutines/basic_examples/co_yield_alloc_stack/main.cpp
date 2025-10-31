d5969a8a0228576465a92612a97375659d131044
Vladislav Antonov <vladislav.antonov@gmail.com>
2025-10-31 21:25:30 +0200

    Add co_yield stack alloc sample

---
+++ coroutines/basic_examples/co_yield_alloc_stack/main.cpp
@@ -0,0 +1,130 @@
+#include <cassert>
+#include <coroutine>
+#include <iostream>
+#include <source_location>  // ScopedLogger
+#include <utility>
+
+// https://github.com/philsquared/cpp_coroutines_helpers/
+// https://github.com/philsquared/understanding_cpp_coroutines
+
+// Logs to cout, with current indent
+template <typename... Args>
+void log(Args&&... args);
+
+struct ScopedLogger {
+  inline static int indent;
+
+  std::string m_name;
+
+  ScopedLogger(
+      std::string name = std::source_location::current().function_name())
+      : m_name(std::move(name)) {
+    log("\\ ", m_name, '\n');
+    indent++;
+  }
+  ~ScopedLogger() {
+    indent--;
+    log("/ ", m_name, '\n');
+  }
+};
+
+template <typename... Args>
+void log(Args&&... args) {
+  std::cout << std::string(ScopedLogger::indent, ' ');
+  (std::cout << ... << std::forward<Args>(args));
+}
+
+// Creates a ScopedLogger for the current function
+#define LOGF() ScopedLogger logger##__LINE__
+
+struct Buffer {
+  char data[200];
+};
+
+struct Task {
+  struct promise_type {
+    Task get_return_object() {
+      LOGF();
+      return Task{std::coroutine_handle<promise_type>::from_promise(*this)};
+    }
+
+    auto initial_suspend() {
+      LOGF();
+      return std::suspend_always{};
+    }
+
+    auto final_suspend() noexcept {
+      LOGF();
+      return std::suspend_always{};
+    }
+
+    void unhandled_exception() {
+      LOGF();
+      std::abort();
+    }
+
+    auto yield_value(int aValue) {
+      LOGF();
+      mYieldedValue = aValue;
+      return std::suspend_always{};
+    }
+
+    void return_void() { LOGF(); }
+
+    int mYieldedValue{};
+
+    void* operator new(std::size_t sz, int arg1, int arg2, Buffer& buffer) {
+      LOGF();
+      log("arguments: ", arg1, " ", arg2, '\n');
+      log("size: ", sz, '\n');
+      assert(sizeof(buffer.data) >= sz);
+      return buffer.data;
+    }
+
+    void operator delete(void* ptr, std::size_t sz) { LOGF(); }
+  };
+
+  // Cotrols and destroys coroutines
+  // Obtained by a promise
+  std::coroutine_handle<promise_type> handle;
+
+  ~Task() {
+    LOGF();
+    if (handle) {
+      handle.destroy();
+    }
+  }
+
+  auto operator=(Task&&) = delete;
+  auto operator=(const Task&) = delete;
+
+  int get_yielded_value() const { return handle.promise().mYieldedValue; }
+
+  // Updated to verify there are values to yield
+  bool resume() {
+    handle.resume();
+    return handle.done();
+  }
+};
+
+Task f(int repetitions, int extra, Buffer&) {
+  LOGF();
+  int a = 0;
+  int b = 1;
+  co_yield a;
+  while (repetitions-- > 0) {
+    co_yield (a = std::exchange(b, a + b)) + extra;
+  }
+}
+
+int main() {
+  LOGF();
+  Buffer buffer;
+  auto task = f(5, 1, buffer);
+
+  while (!task.resume()) {
+    log("YieldedValue ", task.get_yielded_value(), '\n');
+  };
+
+  return 0;
+}
